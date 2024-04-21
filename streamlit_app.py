4f02a7b8566fa18430885420132c567514b85870
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-04-21 11:41:58 +0530

    speed se push kiya jyada koi kaam nahi hai

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import os,time
 import streamlit as st
 @st.cache_resource
 def init_connection1():
-	return os.system("/home/adminuser/venv/bin/python shubhem_bot.py")
+	return os.system("/home/adminuser/venv/bin/python main.py")
 _=init_connection1()
